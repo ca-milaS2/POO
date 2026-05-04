b20f144c6ea77d111a78ba2e9c7fa0c2682b94f7
Camila Yuki <camila.yuki.cezar@gmail.com>
2026-05-04 09:24:03 -0300

    Add files via upload

---
+++ prjExemploGuiRetangulo/src/fatec/poo/model/Retangulo.java
@@ -0,0 +1,47 @@
+package fatec.poo.model;
+
+/**
+ *
+ * @author Yuki
+ */
+public class Retangulo {
+    private double altura;
+    private double base;
+    private String unidadeMedida;
+
+    public Retangulo(String uniMed){
+        unidadeMedida = uniMed;
+    }
+    
+    public void setAltura(double a){
+        altura = a;
+    }  
+    
+    public double getAltura(){
+        return(altura);
+    }
+    
+    public void setBase(double b){
+        base = b;
+    }  
+    
+    public double getBase(){
+        return(base);
+    }
+   
+    public String getUnidadeMedida(){
+        return(unidadeMedida);
+    }
+    
+    public double calcArea(){
+        return(altura * base);
+    }
+    
+    public double calcPerimetro(){
+        return(2*(altura + base));
+    }   
+    
+    public double calcDiagonal(){
+        return(Math.sqrt(Math.pow(base, 2) + Math.pow(altura, 2)));
+    }
+}
